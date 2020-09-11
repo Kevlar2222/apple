846a2f968850bb04bce24331c11a6ec7387be99e
Kevlar2222 <kevin.satti@hotmail.co.uk>
2020-09-11 17:28:23 +0800

    Updates

---
+++ precache-manifest.7ac039fb5d5938967cba977e2c4ec04e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cdbdc8e3adde1d0bfa9fc802c4f7f78",
+    "url": "/apple/index.html"
+  },
+  {
+    "revision": "ed322806313f6b67024f",
+    "url": "/apple/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "42cc372ec8c16fbe656c",
+    "url": "/apple/static/js/2.b772e020.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apple/static/js/2.b772e020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed322806313f6b67024f",
+    "url": "/apple/static/js/main.61435812.chunk.js"
+  },
+  {
+    "revision": "0d082bfb39a84612276b",
+    "url": "/apple/static/js/runtime-main.acc50272.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/apple/static/media/logo.5d5d9eef.svg"
+  }
+]);
